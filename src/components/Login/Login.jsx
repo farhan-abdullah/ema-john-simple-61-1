b86e0b6973e5528344d3e4c7c1968df5d75d9df9
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-04-25 20:11:52 +0200

    61-1.5

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import './Login.css';
 const Login = () => {
-    return (
-        <div>
-            <h2>Login coming sooon</h2>
-        </div>
-    );
+	return (
+		<div className='form-container'>
+			<h2 className='form-title'>Login</h2>
+			<form>
+				<div className='form-control'>
+					<label htmlFor=''>Email</label>
+					<input type='email' name='email' id='' required />
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBIEs_H4vF0w0QeXSNI0It8nk6iEldYgDU',
+	authDomain: 'ema-john-simple-61-1.firebaseapp.com',
+	projectId: 'ema-john-simple-61-1',
+	storageBucket: 'ema-john-simple-61-1.appspot.com',
+	messagingSenderId: '336651860128',
+	appId: '1:336651860128:web:6df63a7026bf4b26f831c5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
